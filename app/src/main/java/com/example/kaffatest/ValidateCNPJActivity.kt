370157f57f174047baeff4bd0b9efea17f11adbc
fulanuc <19h9bgt9>
2021-05-30 16:16:15 -0300

    First excercise now flags a given cnpj string that has a number of characters different from 18 and different from 14 as straight up invalid now.
    Fixes alongside the conditioners, maintaned some but merged like half of the logic checks into a single check in most of the scenarios.
    
    00000000000000 is now invalid.
    44444444444444 is now invalid.

--- app/src/main/java/com/example/kaffatest/ValidateCNPJActivity.kt
+++ app/src/main/java/com/example/kaffatest/ValidateCNPJActivity.kt
@@ -18,31 +18,31 @@ class ValidateCNPJActivity : AppCompatActivity() {
     }
     //01.345.789/1234-67
     //12.345.678/9012-34
-    //11.444.777/0001-00
+    //11.444.777/0001-00 -> last digits are invalid
+    //11.444.777/0001-61 -> corrected last digits
 
-    fun verify(view: View) {
+    fun verify(view: View) {                        //identifies if the given cnpj is formated in a specific manner.
         val cnpj = editText.text.toString()
-        var cnpjValue: String
+        val cnpjValue: String
         if (cnpj.length == 18 &&
             cnpj[2] == '.' &&
             cnpj[6] == '.' &&
             cnpj[10] == '/' &&
             cnpj[15] == '-' ) {
-            cnpjValue = cnpj.substring(0, 2) +
-                    cnpj.substring(3, 6) +
-                    cnpj.substring(7, 10) +
-                    cnpj.substring(11, 15) +
-                    cnpj.substring(16, 18)
-            try {
-                parseLong(cnpjValue)
-            } catch (e: NumberFormatException) {
-                errorDisplay()
-                return
-            }
+            cnpjValue = cnpj.substring(0, 2)   +
+                        cnpj.substring(3, 6)   +
+                        cnpj.substring(7, 10)  +
+                        cnpj.substring(11, 15) +
+                        cnpj.substring(16, 18)
         }
         else
             cnpjValue = cnpj
-
+        try {
+            parseLong(cnpjValue)                     //searches the given cnpj looking after non-numerals
+        } catch (e: NumberFormatException) {
+            showNotValid()
+            return
+        }
         if (cnpjValue.length == 14) {
             val authDigitOne = cnpjValue.substring(0, 12)
             val weightDigitOne = "543298765432"
@@ -50,80 +50,75 @@ class ValidateCNPJActivity : AppCompatActivity() {
             weightDigitOne.indices.forEach { i ->
                 sumDigitOne += authDigitOne[i].digitToInt() * weightDigitOne[i].digitToInt()
             }
+
             Log.d("digitcheck1", "valor de sumDigitOne: ${sumDigitOne}")
+
             sumDigitOne %= 11
-            if (sumDigitOne < 2){
-                if (cnpjValue[13].digitToInt() == 0) {
-//                  proceeds to authDigitTwo
-//                  digitTwoVerify(cnpjValue)
-                    Log.d("ValidateCNPJActivity", " cnpjValue.lengh = ${cnpjValue.length}")
-                    val authDigitTwo = cnpjValue.substring(0, 13)
-                    val weightDigitTwo = "6543298765432"
-                    var sumDigitTwo = 0
-                    var counterre = 0
-                    weightDigitTwo.indices.forEach { i ->
-                        sumDigitTwo += authDigitTwo[i].digitToInt() * weightDigitTwo[i].digitToInt()
-                    }
-                    Log.d("digitcheck2", "valor de sumDigitTwo: ${sumDigitTwo}")
-                    sumDigitTwo %= 11
-                    if (sumDigitTwo < 2) {
-                        if (cnpjValue.substring(13, 14).toInt() == 0)
-//                                  is a CNPJ
-                            isValidDisplay()
-                        else {
-                            errorDisplay()
-                            return
-                        }
-                    }
-                    else if (cnpjValue.substring(13, 14).toInt() == 11 - sumDigitTwo)
-//                                  is a CNPJ
-                            isValidDisplay()
-                    else{
-                        errorDisplay()
-                        return
-                    }
+            if (sumDigitOne < 2 && cnpjValue[12].digitToInt() == 0) {
+//              given conditions are met: proceeds to authenticate the second rule digit
+                Log.d("ValidateCNPJActivity", " cnpjValue.lengh = ${cnpjValue.length}")
+                val authDigitTwo = cnpjValue.substring(0, 13)
+                val weightDigitTwo = "6543298765432"
+                var sumDigitTwo = 0
+                weightDigitTwo.indices.forEach { i ->
+                    sumDigitTwo += authDigitTwo[i].digitToInt() * weightDigitTwo[i].digitToInt()
+                }
+
+                Log.d("digitcheck2", "valor de sumDigitTwo: ${sumDigitTwo}")
+
+                sumDigitTwo %= 11
+                if (sumDigitTwo < 2 && cnpjValue[13].digitToInt() == 0)
+//                  is a CNPJ
+                    showIsValid()
+                else if (cnpjValue[13].digitToInt() == 11 - sumDigitTwo)
+//                  is a CNPJ
+                    showIsValid()
+                else{
+                    showNotValid()
+                    return
                 }
             }
             else if (cnpjValue[12].digitToInt() == 11 - sumDigitOne){
-//                  proceeds to authDigitTwo
-//                  digitTwoVerify(cnpjValue)
-                    val authDigitTwo = cnpjValue.substring(0, 14)
-                    val weightDigitTwo = "6543298765432"
-                    var sumDigitTwo = 0
-                    weightDigitTwo.indices.forEach { i ->
-                        sumDigitTwo += authDigitTwo[i].digitToInt() * weightDigitTwo[i].digitToInt()
-                    }
-                    Log.d("digitcheck2", "valor de sumDigitTwo: ${sumDigitTwo}")
-                    sumDigitTwo %= 11
-                    if (sumDigitTwo < 2) {
-                        if (cnpjValue[13].digitToInt() == 0)
-//                               is a CNPJ
-                            isValidDisplay()
-                        else{
-                            errorDisplay()
-                            return
-                        }
-                    }
-                    else if (cnpjValue[13].digitToInt() == 11 - sumDigitTwo)
-//                              is a CNPJ
-                        isValidDisplay()
-                    else {
-                        errorDisplay()
-                        return
-                    }
+//              proceeds to authenticate the second rule digit
+                val authDigitTwo = cnpjValue.substring(0, 13)
+                val weightDigitTwo = "6543298765432"
+                var sumDigitTwo = 0
+                weightDigitTwo.indices.forEach { i ->
+                    sumDigitTwo += authDigitTwo[i].digitToInt() * weightDigitTwo[i].digitToInt()
+                }
+
+                Log.d("digitcheck2", "valor de sumDigitTwo: ${sumDigitTwo}")
+
+                sumDigitTwo %= 11
+                if (sumDigitTwo < 2 && cnpjValue[13].digitToInt() == 0) {
+//                  is a CNPJ
+                    showIsValid()
+                }
+                else if (cnpjValue[13].digitToInt() == 11 - sumDigitTwo)
+//                  is a CNPJ
+                    showIsValid()
+                else {
+                    showNotValid()
+                    return
+                }
             }
-        else {
-            errorDisplay()
-            return
+            else {
+                showNotValid()
+                return
             }
         }
+        else {
+            showNotValid()
+            return
+        }
+
     }
 
-    fun errorDisplay() {
+    fun showNotValid() {
         editText.error = "Invalid CNPJ"
     }
 
-    fun isValidDisplay() {
+    fun showIsValid() {
         Toast.makeText(this, "This CNPJ is Valid.", Toast.LENGTH_SHORT).show()
     }
 }
